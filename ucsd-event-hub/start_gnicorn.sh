04dbfc3a87386523919ccee5f1b4e2464b0e2b5e
Ubuntu <ubuntu@ip-172-31-3-209.us-west-1.compute.internal>
2017-11-08 00:57:24 +0000

    Adding addons needed for basic development

---
+++ ucsd-event-hub/start_gnicorn.sh
@@ -0,0 +1,19 @@
+APNAME=event_hub
+APPDIR=/home/ubuntu/$APPNAME/
+
+LOGFILE=$APPDIR'gunicorn.log'
+ERRORFILE=$APPFIR'gunicorn-error.log'
+
+NUM_WORKERS=3
+
+ADDRESS=127.0.0.1:8000
+
+cd $APPDIR
+
+source ~/.bashrc
+#workon $APPNAME
+
+exec gunicorn event_hub.wsgi.application \
+-w $NUM_WORKERS --bind=$ADDRESS \
+--log-level=debug \
+--log-file=$LOGFILE 2>>$LOGFILE  1>>$ERRORFILE &
