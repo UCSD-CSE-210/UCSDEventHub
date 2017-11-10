1df629d0cb684eb1cd03845bf50631944f03dc6c
devyani <devkul.yani@gmail.com>
2017-11-09 18:53:30 -0800

    homepage django changes

--- hub/views.py
+++ hub/views.py
@@ -4,7 +4,13 @@ from .models import Event
 # Create your views here.
 def post_list(request):
     events = Event.objects.all()
-    return render(request, 'hub/post_list.html', {'events':events})
+    return render(request, 'hub/Homepage.html', {'events':events})
+
+def event_list(request):
+    events1 = Event.objects.all()
+    events = events1 #.sort(key=lambda e: e.date)[:3]
+    return render(request, 'hub/Homepage.html', {'events':events})
+
     
 def event_upload(request):
     return render(request, 'hub/event_upload.html', {})
