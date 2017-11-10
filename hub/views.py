3daab643ae860479ba53557412abf8fe47c974b0
chandanatl <tlchandana@gmail.com>
2017-11-10 14:54:13 -0800

    Add calendar implemented in event details page

--- hub/views.py
+++ hub/views.py
@@ -23,27 +23,12 @@ def event_upload(request):
     return render(request, 'hub/event_upload.html', {})
 
 def event_detail(request):
-    # making a call to the get event details api with an id of the event to get details. 
-
-    # print eventId
-    # print type(request.GET)
     eventId = request.GET.get("id")
-    # print type(eventId)
     evntId = int(eventId)
-    print evntId
-    print type(evntId)
-    # eventId = json.loads(eventIds)
     events = event_details(evntId)
-    # events[0]
-    # eventDict = {}
-    l=events
-    print l	
-
-  	# print eventDetails
-    # print events.get("description")
-    # return HttpResponse("<h1>Hi</h1>")
-    # return render(request, 'hub/event_details.html', {'description':str(events.get("description"))})
-    return render(request, 'hub/event_details.html', {'events':events[0]})
+    event = events[0]
+    event["googleDate"] = event["start_date"].strftime("%Y%m%dT%H%M%S")+"/"+event["end_date"].strftime("%Y%m%dT%H%M%S") 
+    return render(request, 'hub/event_details.html', {'events':event})
 
 	
 def render_search_page(request):
