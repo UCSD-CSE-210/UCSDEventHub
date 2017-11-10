efac95b094631be941a6617fb8c0845a533891c0
Kulshreshth Dhiman <kdhiman0309@gmail.com>
2017-11-09 20:49:15 -0800

    rendering function for search page | template placeholders

--- hub/views.py
+++ hub/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Event
+from .models import search_events
+import re
 
 # Create your views here.
 def post_list(request):
@@ -18,3 +20,15 @@ def event_upload(request):
 def event_details(request):
     # making a call to the get event details api with an id of the event to get details. 
     return render(request, 'hub/event_details.html', {'events':events})
+	
+def render_search_page(request):
+	search_keywords = request.GET.get("q")
+	def clean(key):
+		key = re.sub(' ', '-', key)
+		key = re.sub('[^A-Za-z0-9-]', '', key)
+		key = re.sub('-+',' ', key)
+		return key
+	events = search_events(search_keywords)
+	return render(request, 'hub/search_page.html', {'events':events})
+	
+	
