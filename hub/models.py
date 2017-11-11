4b80f17a9c5842c890be16e0ce489dd010f35470
Nikhil Yogendra Murali <nyogendr@eng.ucsd.edu>
2017-11-11 13:00:27 -0800

    Added sort function in search_events and upcoming_events API. Fixes #32

--- hub/models.py
+++ hub/models.py
@@ -23,11 +23,13 @@ def search_events(search_text):
     events_results = []
     for keywords in search_text.split():
         events_results += Event.objects.filter(title__icontains=keywords).all().values()
+    events_results.sort(key = lambda item:item['start_date'])
     return events_results
 
 def upcoming_events():
     now_time = timezone.now()
     events_results = list(Event.objects.filter(start_date__gte=now_time).all().values())
+    events_results.sort(key = lambda item:item['start_date'])
     return events_results
 
 def event_details(event_id):
