b9bb9997f3a2508e3746f7be9e44c51948a61f96
Sai Chaitanya Kolasani <kolasanichaitanya@gmail.com>
2017-11-18 20:03:17 -0800

    Refactored the event upload controller

--- hub/urls.py
+++ hub/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 from . import views
 from .views import SearchListing
+from .views import EventUpload
 urlpatterns = [
     url(r'^$', views.event_list, name='event_list'),
-    url(r'^event_upload/', views.event_upload, name='event_upload'),
+    url(r'^'+EventUpload.base_url, EventUpload.render_page, name=EventUpload.name),
     url(r'^event_detail/', views.event_detail, name='event_detail'),
     url(r'^'+SearchListing.base_url, SearchListing.render_page, name=SearchListing.name),
-    url(r'^submit_event/', views.submit_event, name='submit_event'),
+    url(r'^'+EventUpload.submit_url, EventUpload.event_upload_handler, name=EventUpload.submit_view_name)
 ]
--- hub/views.py
+++ hub/views.py
@@ -17,20 +17,20 @@ class Utils():
 	@staticmethod
 	def format_day(date):
 		return date.strftime("%a, %b %d")
-	
+
 	@staticmethod
 	def format_time(date):
 		return date.strftime("%I:%M %p")
-	
+
 	@staticmethod
 	def format_date(date):
 		return Utils.format_day(date)+" "+Utils.format_time(date)
-	
+
 	@staticmethod
 	def get_image_url(image):
 		return Constants.media_path+image
-	
-		
+
+
 class Constants():
 	app_name="hub"
 	media_path="/media/"
@@ -44,9 +44,6 @@ def event_list(request):
     return render(request, 'hub/Homepage.html', {'events':events})
 
 
-def event_upload(request):
-    return render(request, 'hub/event_upload.html', {})
-
 def event_detail(request):
     eventId = request.GET.get("id")
     evntId = int(eventId)
@@ -56,18 +53,71 @@ def event_detail(request):
     event["start_day"] = event["start_date"].strftime("%a, %b %d")
     event["start_time"] = event["start_date"].strftime("%I:%M %p")
     event["image_url"] = "/media/"+event["image"]
-	
     return render(request, 'hub/event_details.html', {'events':event})
 
 
-	
+class EventUpload():
+	name = "event_upload_form"
+	base_url = "event_upload/"
+	submit_url = "submit_event/"
+	submit_view_name = "submit_event"
+	template = Constants.app_name+"/event_upload.html"
+
+	# Event Upload related views
+	def _get_alert_html(self,text,alerttype):
+		div=""
+		if alerttype == 'fail':
+			div = '<div id="valErr" class="row viewerror clearfix">\n'
+			div = div + '   <div class="alert alert-danger">'+text+'</div>\n'
+			div = div+'</div>'
+		else:
+			div = '<div id="valErr" class="row viewerror clearfix">\n'
+			div = div + '   <div class="alert alert-success">'+text+'</div>\n'
+			div = div+'</div>'
+		return div
+
+	def _render_event_upload(self,request):
+		return render(request, self.template, {'div_elem': " "})
+
+	def _submit_event(self,request):
+		data = request.POST.items()
+		for key, value in data:
+			print("%s %s" % (key, value))
+
+		# Build the events
+		new_event = Event()
+		new_event.title = request.POST.get('n_title',"")
+		new_event.contact_email = request.POST.get('n_email',"")
+		new_event.organizer = request.POST.get('n_org',"")
+		new_event.location = request.POST.get('n_loc',"")
+		new_event.description = request.POST.get('n_title',"n_desc")
+		startdate_object = datetime.strptime(request.POST.get('n_startdate'),'%m/%d/%Y %I:%M %p')
+		enddate_object = datetime.strptime(request.POST.get('n_enddate'),'%m/%d/%Y %I:%M %p')
+		new_event.start_date = startdate_object
+		new_event.end_date = enddate_object
+		uploaded_poster = request.FILES['n_uploadedposter']
+		new_event.image = uploaded_poster
+		new_event.hashtags = request.POST.get('n_tags')
+		add_event_to_db(new_event)
+		return render(request, 'hub/event_upload.html', {'div_elem':self._get_alert_html('Event Upload is Successful','sucess')})
+
+	@staticmethod
+	def render_page(request):
+		module = EventUpload()
+		return module._render_event_upload(request)
+
+	@staticmethod
+	def event_upload_handler(request):
+		module = EventUpload()
+		return module._submit_event(request)
+
+
 class SearchListing():
 	name = "search_page"
 	base_url = "event_search/"
 	template = Constants.app_name+"/search_page.html"
 		
 	class Response():
-		
 		def __init__(self):
 			self.events = []
 			self.empty_search = True
@@ -114,7 +164,7 @@ class SearchListing():
 		self._generate_repsonse(events)
 		
 		return self._render_me(request)
-	
+
 	@staticmethod
 	def render_page(request):
 		module = SearchListing()
@@ -122,46 +172,5 @@ class SearchListing():
 	
 	def build_url(self, keywords):
 		return base_url+"?"+keywords
-	
-	
-	
-# Event Upload related views
-def get_alert(text,alerttype):
-	div=""
-	if alerttype == 'fail':
-		div = '<div id="valErr" class="row viewerror clearfix">\n'
-		div = div + '   <div class="alert alert-danger">'+text+'</div>\n'
-		div = div+'</div>'
-	else:
-		div = '<div id="valErr" class="row viewerror clearfix">\n'
-		div = div + '   <div class="alert alert-success">'+text+'</div>\n'
-		div = div+'</div>'
-	return div
-
-def event_upload(request):
-    return render(request, 'hub/event_upload.html', {'div_elem': " "})
-
-def submit_event(request):
-	data = request.POST.items()
-	for key, value in data:
-		print("%s %s" % (key, value))
-
-	# Build the events
-	new_event = Event()
-	new_event.title = request.POST.get('n_title',"")
-	new_event.contact_email = request.POST.get('n_email',"")
-	new_event.organizer = request.POST.get('n_org',"")
-	new_event.location = request.POST.get('n_loc',"")
-	new_event.description = request.POST.get('n_title',"n_desc")
-	startdate_object = datetime.strptime(request.POST.get('n_startdate'),'%m/%d/%Y %I:%M %p')
-	enddate_object = datetime.strptime(request.POST.get('n_enddate'),'%m/%d/%Y %I:%M %p')
-	new_event.start_date = startdate_object
-	new_event.end_date = enddate_object
-
-	uploaded_poster = request.FILES['n_uploadedposter']
-	new_event.image = uploaded_poster
-	new_event.hashtags = request.POST.get('n_tags')
-	add_event_to_db(new_event)
-	return render(request, 'hub/event_upload.html', {'div_elem':get_alert('Event Upload is Successful','sucess')})
 
 
