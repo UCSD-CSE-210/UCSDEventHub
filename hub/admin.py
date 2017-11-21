6d51c3342436302fa71801fea93de6704c75680a
Nikhil Yogendra Murali <nyogendr@eng.ucsd.edu>
2017-11-21 15:39:46 -0800

    Added UserProfile, OrganizationDetails classes in model and added them to show up during user creation in admin page. Fixes #41 #42

--- hub/admin.py
+++ hub/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from .models import Event
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Event, UserProfile, OrganizationDetails
 # Register your models here.
 
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'User Profile'
+    fk_name = 'user'
+
+class OrganizationDetailsInline(admin.StackedInline):
+    model = OrganizationDetails
+    can_delete = False
+    verbose_name_plural = 'Organization Details'
+    fk_name = 'organization'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [ProfileInline, OrganizationDetailsInline, ]
+
+    # def get_inline_instances(self, request, obj=None):
+    #     if not obj:
+    #         return list()
+    #     return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
 admin.site.register(Event)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- hub/models.py
+++ hub/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import pytz
 
 # Create your models here.
@@ -17,3 +21,26 @@ class Event(models.Model):
     # modify_user = models.CharField(max_length = 255, validators=[MinLengthValidator(1)])
     # modify_date = models.DateTimeField(null = False)
     # delete_date = models.DateTimeField(null = True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete = models.CASCADE, primary_key=True)
+    user_is_organization = models.BooleanField(default = False)
+    user_image = models.ImageField(
+        upload_to="users", default=None, null = True, blank = True)
+
+class OrganizationDetails(models.Model):
+    organization = models.OneToOneField(
+        User, on_delete = models.CASCADE, primary_key=True)
+    description = models.TextField(null = True, blank=True)
+    contact_first_name = models.CharField(max_length = 255, null = False)
+    contact_last_name = models.CharField(max_length = 255, null = False)
+    contact_email = models.EmailField(null = False)
+    address = models.TextField(null = True, blank=True)
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: "\
+                + "'+999999999'. Up to 15 digits allowed.")
+    # Using E.164 as recommended phone number format
+    contact_number = models.CharField(
+        validators = [phone_regex], max_length = 15, blank = True, null=True)
